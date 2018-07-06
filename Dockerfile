0fc6f4d915cbd6909e9bd362fc72cd0801903397
James S <jamessteele@ufl.edu>
2018-07-06 14:28:29 -0400

    Get docker working

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM elixir:alpine
+FROM elixir:latest
+RUN apt-get update -qq && apt-get install -y telnet
 ADD . /mmo
 WORKDIR /mmo/mmo
+EXPOSE 4500
--- mmo/lib/mmo.ex
+++ mmo/lib/mmo.ex
@@ -1,18 +1,48 @@
+require Logger
+
 defmodule MMO do
   @moduledoc """
   Documentation for MMO.
   """
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    {:ok, socket} = port
+      |> :gen_tcp.listen([
+        :binary,
+        packet: :line,
+        active: false,
+        reuseaddr: true
+      ])
 
-  ## Examples
+    Logger.info("Now listening on port #{port}")
+    listener(socket)
+  end
 
-      iex> MMO.hello
-      :world
+  defp listener(socket) do
+    {:ok, client} = socket
+      |> :gen_tcp.accept
 
-  """
-  def hello do
-    :world
+    serve(client)
+    listener(socket)
+  end
+
+  defp serve(socket) do
+    socket
+      |> read_line
+      |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = socket
+      |> :gen_tcp.recv(0)
+
+    data
+  end
+
+  defp write_line(line, socket) do
+    socket
+      |> :gen_tcp.send('hello')
   end
 end
